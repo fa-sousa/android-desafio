3b30d5fcca735f9bdd9459004b510ac8c800297c
fa-sousa <fsousa292@gmail.com>
2020-07-03 15:45:00 -0300

    Criação de nova activity para adicionar recyclerview

--- DesafioFleury/app/src/main/java/com/example/desafiofleury/activity/MainActivity.kt
+++ DesafioFleury/app/src/main/java/com/example/desafiofleury/activity/MainActivity.kt
@@ -1,49 +1,13 @@
 package com.example.desafiofleury.activity
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.desafiofleury.R
-import com.example.desafiofleury.model.Exames
-import com.example.desafiofleury.service.Endpoint
-import com.example.desafiofleury.service.NetworkUtils
-import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        getData()
-    }
-
-    fun getData() {
-        val retrofitClient = NetworkUtils
-                //sem o "s" a chamada não acontece (questão de segurança)
-            .getRetrofitInstance("https://5bfbefa5cf9d29001345c529.mockapi.io/api/v1/")
-
-        val endpoint = retrofitClient.create(Endpoint::class.java)
-        val callback = endpoint.getResults()
-
-        callback.enqueue(object : Callback<List<Exames>> {
-            override fun onFailure(call: Call<List<Exames>>, t: Throwable) {
-                Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
-            }
-
-            //itens do array de objeto do json
-            override fun onResponse(call: Call<List<Exames>>, response: Response<List<Exames>>) {
-                response.body()?.forEach {
-                    textView.text = textView.text.toString().plus(it.name)
-                    textView.text = textView.text.toString().plus(it.doctor)
-                    textView.text = textView.text.toString().plus(it.healthy)
-                    textView.text = textView.text.toString().plus(it.id)
-                }
-            }
-        })
-
     }
 }
