5f525d3cfbd3ff6b3e56d71848394f8df5466853
fa-sousa <fsousa292@gmail.com>
2020-07-05 19:00:07 -0300

    Adiciona campo de pesquisa no recyclerview

--- DesafioFleury/app/src/main/java/com/example/desafiofleury/activity/ExamesActivity.kt
+++ DesafioFleury/app/src/main/java/com/example/desafiofleury/activity/ExamesActivity.kt
@@ -1,6 +1,7 @@
 package com.example.desafiofleury.activity
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
